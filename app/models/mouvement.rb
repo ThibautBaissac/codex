30ab27203cd6fa2d420d5e8a1474b9ec83747b3d
ThibautBaissac <tbaissac@gmail.com>
2023-04-23 11:30:53 +0200

    Fix delete mouvement

--- app/models/mouvement.rb
+++ app/models/mouvement.rb
@@ -1,6 +1,6 @@
 class Mouvement < ApplicationRecord
   belongs_to :work
-  has_many :mouvement_elements
+  has_many :mouvement_elements, dependent: :destroy
   has_many :elements, through: :mouvement_elements
 
   validates :title, :number, presence: true
