19056137bead7253eea8f335825cfc83f484bdc0
ThibautBaissac <tbaissac@gmail.com>
2023-04-27 18:18:07 +0200

    Fix my_format (helper)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,15 +35,6 @@ class ApplicationController < ActionController::Base
     render :search
   end
 
-
-  def my_format(text)
-    text.gsub!(/(\*)([^*]+?)(\*)/, '<b>\2</b>') # *text* > <b>text</b>
-    text.gsub!(/(_)([^_]+?)(_)/, '<em>\2</em>') # _text_ > <em>text</em>
-    text.gsub!(/(~)([^~]+?)(~)/, '<s>\2</s>') # ~text~ >  <s>text</s>
-    text.gsub!(/\/n/, '<br>') # <br>
-    text.html_safe
-  end
-
   private
 
   def user_not_authorized
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def my_format(text)
+    text.gsub!(/(\*)([^*]+?)(\*)/, '<b>\2</b>') # *text* > <b>text</b>
+    text.gsub!(/(_)([^_]+?)(_)/, '<em>\2</em>') # _text_ > <em>text</em>
+    text.gsub!(/(~)([^~]+?)(~)/, '<s>\2</s>') # ~text~ >  <s>text</s>
+    text.gsub!(/\/n/, '<br>') # <br>
+    text.html_safe
+  end
 end
