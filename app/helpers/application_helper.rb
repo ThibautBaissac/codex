7f13d5c14c0eeb88244b71d89be61cd2ed49b930
ThibautBaissac <tbaissac@gmail.com>
2023-04-29 14:18:59 +0200

    change my_format > simple_format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def my_format(text)
-    text.gsub!(/(\*)([^*]+?)(\*)/, '<b>\2</b>') # *text* > <b>text</b>
-    text.gsub!(/(_)([^_]+?)(_)/, '<em>\2</em>') # _text_ > <em>text</em>
-    text.gsub!(/(~)([^~]+?)(~)/, '<s>\2</s>') # ~text~ >  <s>text</s>
-    text.gsub!(/\/n/, '<br>') # <br>
-    text.html_safe
-  end
 end
