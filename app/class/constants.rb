915634f39ef9dcb5c69bff8af101df2d78a3a342
ThibautBaissac <tbaissac@gmail.com>
2023-04-29 13:32:56 +0200

    add instruments

--- app/class/constants.rb
+++ app/class/constants.rb
@@ -1,6 +1,30 @@
 class Constants
   module Instrumentation
-    LIST = %i[voice piano soprano baryton].freeze
+    LIST = %i[voice
+      piano
+      soprano
+      baryton
+      saxo_alto
+      violin
+      cello
+      alto
+      oboe
+      harpsichord
+      mezzo_sopprano
+      clarinet_b_flat
+      percussion
+      two_pianos
+      horn
+      bassoon
+      flute
+      english_horn
+      mixed_choir
+      orchestra
+      small_flute
+      two_violins
+      harp
+      celesta
+      synthesizer].freeze
   end
 
   module Element
