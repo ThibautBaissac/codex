273c30d7dbb7d4ba934e6ce2a401e2a08064b087
ThibautBaissac <tbaissac@gmail.com>
2023-05-01 15:49:26 +0200

    add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "works#index"
+  root to: "static_pages#index"
   get :search, to: "application#search"
   resources :works do
     post :add_selected_element, on: :member
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :elements
   resources :metrics, only: %i[index]
   resources :concerts
+  resources :static_pages, only: %i[index]
   resources :sessions, only: %i[new create destroy]
   get '/signout', to: 'sessions#destroy', as: :signout
 end
