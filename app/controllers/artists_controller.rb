3d7bafcc3eb965d2e51b3c277ee39d5d9c128753
thibautbaissac <tbaissac@gmail.com>
2023-12-15 15:06:45 +0100

    add artists and writings routes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,9 @@ class ArtistsController < ApplicationController
   def index
     @pagy, @artists = pagy(Artist.all)
   end
+
+  def show
+    @artist = Artist.find(params[:id]).decorate
+    @pagy, @writings = pagy(@artist.writings, items: 20)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "artists#index"
   get 'artists/index'
 
+  resources :artists do
+    resources :writings
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
