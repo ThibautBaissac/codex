a27695fdc28afe5d5c6d5b3559c1109581bbece8
thibautbaissac <tbaissac@gmail.com>
2023-12-27 16:41:42 +0100

    First i18n implementation

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module Writings
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :fr
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
