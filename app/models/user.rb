c5f79a307605ae1c75fb74d2b979bc2c77eb52a3
thibautbaissac <tbaissac@gmail.com>
2023-12-29 15:20:52 +0100

    Add email and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   has_many :annotations
 
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: Constants::User::REGEX_EMAIL
   normalizes :email, with: ->(email) { email.downcase.strip }
+
+  validates :password, presence: true
+  validates_format_of :password, with: Constants::User::PASSWORD_REGEX
 
   generates_token_for :password_reset, expires_in: 15.minutes do
     password_salt&.last(10)
---
+++ lib/constants/user.rb
@@ -0,0 +1,6 @@
+module Constants
+  module User
+    REGEX_EMAIL = /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+\z/
+    PASSWORD_REGEX = /\A^.*(?=.{12,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@:,\-?*_()!#$%^&+=]).*$\z/
+  end
+end
