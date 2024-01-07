dc1957bdeea772b241783fd17376d0258efca7c9
thibautbaissac <tbaissac@gmail.com>
2024-01-07 17:07:24 +0100

    Update primary color

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="modal"
 export default class extends Controller {
   connect() {
-    console.log("modal controller initialized")
     this.element.classList.remove('hidden');
   }
 
