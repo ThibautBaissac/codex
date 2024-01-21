ff24e814195e895953c466db461001bb34e5d90f
Thibaut Baissac <tbaissac@proton.me>
2024-01-21 17:14:21 +0100

    remove AddRoleToUsers migration

--- db/migrate/20240121142033_add_role_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddRoleToUsers < ActiveRecord::Migration[7.1]
-  def up
-    create_enum :user_roles, ["user", "admin", "super_admin"]
-    change_table :users do |t|
-      t.enum :role, enum_type: "role", default: "user", null: false
-    end
-  end
-end
