bb103267d6b368eacd3d3f299e37b2084bfd01b9
Thibaut Baissac <tbaissac@proton.me>
2024-01-31 14:30:13 +0100

    Small UI improvments

--- app/controllers/administration/annotations_controller.rb
+++ app/controllers/administration/annotations_controller.rb
@@ -1,10 +1,14 @@
-class Administration::AnnotationsController < ApplicationController
-  def index
-    @pagy, @annotations = pagy(Annotation.all.includes(:rich_text_content, :user).order(updated_at: :desc), items: 20)
-  end
+module Administration
+  class AnnotationsController < ApplicationController
+    def index
+      @pagy, @annotations = pagy(
+        Annotation.where(user: current_user).includes(:rich_text_content, :user).order(updated_at: :desc), items: 20
+      )
+    end
 
-  def edit
-    @annotation = Annotation.find(params[:id])
-    @writing = @annotation.annotatable
+    def edit
+      @annotation = Annotation.find(params[:id])
+      @writing = @annotation.annotatable
+    end
   end
 end
