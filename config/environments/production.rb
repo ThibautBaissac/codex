40e3df98fc7ac50bedc582267087e36ecfc6ca37
Thibaut Baissac <tbaissac@proton.me>
2024-02-05 09:32:37 +0100

    fix config.assets.css_compressor for assets:precompile step

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.assets.css_compressor = nil
+  #  It prevents sprockets of using sass mode and SassC gem in assets:precompile step in docker build process
 end
